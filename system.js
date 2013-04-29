f769adeff3f58fb5d34795fd236f7f5c7246b6dc
Brian Levine <bmlevine11@gmail.com>
2013-04-29 15:43:24 -0500

    nearly finished popup and modified js

--- system.js
+++ system.js
@@ -1,31 +1,37 @@
-//this is where we do work son
+// this is where we do work son
 
 // need to keep track of gown types, sizes, and barcodes if possible
 
+var barcode = "";
+var option = document.getElementById("#gown_status");
+alert(option);
 
-// //function toLaundry(){
 
-// 	$('barcode').value = bar_number;
-// 	barcode_change();
-// 	update_gown();
+function toLaundry(){
 
-// //}
+	$('barcode').value = bar_number;
+	barcode_change();
+	update_gown();
 
-// function toInventory(){
-// 	console.log("I cant believe it worked");
+}
 
-// }
+function toInventory(){
+	console.log("I cant believe it worked");
 
-// function toHangar(){
+}
 
-// }
+function toHangar(){
 
-// function gown(number){
+}
+
+function gown(number){
 	
-// }
+}
 
-// $(document).ready(function(){
-// 	$("div").fadeOut('slow',1);
-// });
+$(document).ready(function(){
+	$("div").fadeOut('slow',1);
+});
 $("#number").text("dicks");
+
+$("#c3_m").text("boosh")
 
